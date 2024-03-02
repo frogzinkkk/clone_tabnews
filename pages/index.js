26e35c024456f4d58d51755c762d7a22dedd7079
Andre Luis <142546521+frogzinkkk@users.noreply.github.com>
2024-03-02 02:54:56 +0000

    alterando frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
-function Home(){
-    return <h1>Teste 02</h1>
-    
-}
-
-export default Home;
+function Home() {
+    return (
+      <>
+        <h1>"É patético desistir de algo sem nem mesmo ter tentado."</h1>
+        <h3>Alguem que você gosta muito um dia disse isso. Foi o luffy, e eu sei que 
+            você ainda tem muita coisa para se provar e muitos objetivos para se consquistar.
+            Há coisas que não controlamos e que são dificeis de ignorar, principalmente quando envolve
+            alguem ou algo que amamos, e neste caso apenas demonstramos nosso amor e esperamos que tudo fique bem
+            para que possamos compartilhar nossas conquistas com esta pessoa. </h3>
+            <h3>Vai ficar tudo bem meu amor, e voce ainda tem muito do que provar . Ainda ha muitas coisas para se tentar
+                e mostrar que assim como seu personagem favorito (acredito eu ), você nunca desiste, muito menos antes de tentar
+                pois você nao é e nunca sera patetica.
+            </h3>
+            <h2>Eu te amo Meu bem, Bom dia!!!💖</h2>
+      </>
+    );
+  }
+  
+  export default Home;
+  
